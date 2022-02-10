2e7a0a239d3313b526c0a46b916594c1b6eae3d5
sistiri <sistiri@yahoo.com>
2022-02-10 11:36:06 +0100

    Sending a POST Request to FireBase API & Transform Response Data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
-import AddMovie from './components/AddMovie';
+import AddMovie from "./components/AddMovie";
 import "./App.css";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,7 +14,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://star-wars-data-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something went wrong here");
@@ -21,15 +24,27 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = []
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      // const transformedMovies = data.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -40,8 +55,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
+  async function addMovieHandler(movie) {
     console.log(movie);
+    const response = await fetch(
+      "https://star-wars-data-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        heaeders: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies</p>;
@@ -59,14 +86,12 @@ function App() {
   return (
     <React.Fragment>
       <section>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
+        <section>
+          <AddMovie onAddMovie={addMovieHandler} />
+        </section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
